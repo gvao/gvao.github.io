fb8212117c33a417cbd9fd3d3a8dd681ba46450f
Yuri Galvao <yuri.gvao@gmail.com>
2023-11-19 09:36:46 -0300

    refactor menu animation

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,61 +1,30 @@
 const btnHamburger = document.querySelector(".nav__hamburger");
 const navList = document.querySelector(".nav__list");
 
-function CreateMenu(delay = 2) {
-	const component = navList;
-	let interval = null;
-	let seconds = 0;
-	const classNameToShow = "--show";
+const classNameToShow = "--show";
+const delay = 2000
 
-	const isShow = () => component.classList.contains(classNameToShow);
+function debounce(fn, delay) {
+	let timer;
 
-	function intervalCleanup() {
-		if (!interval) return;
-		clearInterval(interval);
-		interval = null;
-        seconds = 0
-	}
-
-	function hideComponent() {
-		component.classList.remove(classNameToShow);
-		intervalCleanup();
-	}
-    
-	function showComponent() {
-        component.classList.add(classNameToShow);
-	}
-    
-	function startToHide() {
-        console.log(`start to hide`);
-        
-        intervalCleanup();
-		interval = setInterval((duration) => {
-            console.log(seconds === duration)
-            if ( seconds === duration) {
-
-                hideComponent();
-                console.log(`hide component`);
-                return;
-            }
-
-            seconds++
-        }, 1000, delay );
-	}
-
-	function toggle() {
-		if (isShow()) return hideComponent();
-		showComponent();
-        startToHide()
-	}
-
-	return {
-		stopHide: intervalCleanup,
-		startToHide,
-		toggle,
+	return () => {
+		clearTimeout(timer);
+		timer = setTimeout(fn, delay);
 	};
 }
-const menu = CreateMenu();
 
-navList.addEventListener("mousemove", menu.stopHide);
-navList.addEventListener("mouseleave", menu.startToHide);
-btnHamburger.addEventListener("click", menu.toggle);
+const isShow = () => navList.classList.contains(classNameToShow);
+
+const hideComponent = () => isShow() && navList.classList.remove(classNameToShow);
+
+const debounceHideMenu = debounce(hideComponent, delay);
+
+const showComponent = () => {
+	if (!isShow()) navList.classList.add(classNameToShow);
+	debounceHideMenu();
+};
+
+const toggle = () => !isShow() && showComponent();
+
+navList.addEventListener("mousemove", showComponent);
+btnHamburger.addEventListener("click", toggle);
